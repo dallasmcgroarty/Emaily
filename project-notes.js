bc4606c9bdd807b618083dc4b150e1d8a6e492f1
Dallas <dallas.mcgroarty@gmail.com>
2024-02-08 14:45:11 -0800

    first commit

---
+++ project-notes.js
@@ -0,0 +1,11 @@
+/**
+ * Project notes
+ *  - react, node, express, mongoDB, stripe
+ */
+
+/**
+ * Express
+ *  - runs in node
+ *  - makes dealing with HTTP traffic easier
+ *  - best node extraction to create back end servers with
+ */
