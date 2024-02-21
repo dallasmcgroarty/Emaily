16c0bf088b3c9c6f52435f3b243660d27d181ffb
Dallas <dallas.mcgroarty@gmail.com>
2024-02-21 15:25:53 -0800

    updated keys and authentication done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 
+require('./models/User');
 require('./services/passport');
-//require('./models/User');
 
 mongoose.connect(keys.mongoURI)
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // require file that returns a function and call it immediately
 require('./routes/authRoutes')(app);
