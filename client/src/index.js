7f325062d8d71ae797459f34dc3443feda24aedb
Dallas <dallas.mcgroarty@gmail.com>
2024-03-04 13:15:32 -0800

    login/logout user added

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import { thunk } from 'redux-thunk';
  
 const el = document.getElementById("root");
  
 const root = ReactDOM.createRoot(el);
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(thunk));
  
 root.render(
   <Provider store={store}>
