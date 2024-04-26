e36037cfe88c0d8ee3c76924fe88890882da18e6
Dallas <dallas.mcgroarty@gmail.com>
2024-04-26 13:14:13 -0700

    testing new key

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,6 @@
 //const passport = require('passport');
 const keys = require('../config/keys');
-const stripe = require('stripe')(keys.stripeSecretKey);
+const stripe = require('stripe')(keys.stripePublishableKey);
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
